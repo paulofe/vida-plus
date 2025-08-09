ecdb6ad6ccede6ed4f6ac3dc5b30d32d216428d8
paulofe <paulofe@gmail.com>
2025-08-09 12:18:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-
-const CACHE_NAME = 'vida-plus-v4.4.0';
+// Vida+ PWA SW v4.4.2
+const CACHE = 'vida-plus-v4.4.2';
 const ASSETS = [
   './',
   './index.html',
@@ -10,32 +10,21 @@ const ASSETS = [
   './icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   const req = event.request;
   event.respondWith(
-    caches.match(req).then(cached => {
-      if (cached) return cached;
-      return fetch(req).then(res => {
-        const respClone = res.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          if(req.method === 'GET' && (new URL(req.url)).origin === self.location.origin) {
-            cache.put(req, respClone).catch(()=>{});
-          }
-        });
-        return res;
-      }).catch(() => caches.match('./index.html'));
-    })
+    caches.match(req).then(cached => cached || fetch(req))
   );
 });
