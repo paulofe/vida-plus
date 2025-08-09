23dc8a9ac23bea55a2351aee17611f753afa51b0
paulofe <paulofe@gmail.com>
2025-08-09 12:38:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Vida+ PWA SW v4.5.0
-const CACHE='vida-plus-v4.5.0';
+const CACHE='vida-plus-v4.5.1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-96.png','./icon-128.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
