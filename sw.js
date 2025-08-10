2a8841a13d67202b25e7da63f73465698426baca
paulofe <paulofe@gmail.com>
2025-08-10 00:46:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW Vida+ 5.2.0
-const CACHE='vida-plus-5.2.0-20250810032823';
+// SW v5.2.1
+const CACHE='vida-plus-5.2.1-20250810034330';
 const SHELL=['index.html','app.js','manifest.webmanifest','icon-96.png','icon-128.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{const req=e.request;e.respondWith(fetch(req).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(req,copy));return r;}).catch(()=>caches.match(req)));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
